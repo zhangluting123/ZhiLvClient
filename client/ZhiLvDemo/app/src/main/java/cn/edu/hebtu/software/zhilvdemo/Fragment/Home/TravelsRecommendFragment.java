fd34ed8629f32cb3a93b2caad9c774e27a3329ed
zhangluting123 <3588652297@qq.com>
2021-02-18 18:03:27 +0800

    add Travels Recommend management

--- client/ZhiLvDemo/app/src/main/java/cn/edu/hebtu/software/zhilvdemo/Fragment/Home/TravelsRecommendFragment.java
+++ client/ZhiLvDemo/app/src/main/java/cn/edu/hebtu/software/zhilvdemo/Fragment/Home/TravelsRecommendFragment.java
@@ -1,5 +1,6 @@
 package cn.edu.hebtu.software.zhilvdemo.Fragment.Home;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,14 +8,27 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +39,8 @@ import cn.edu.hebtu.software.zhilvdemo.Data.Note;
 import cn.edu.hebtu.software.zhilvdemo.DetailActivity.TravelDetailActivity;
 import cn.edu.hebtu.software.zhilvdemo.DetailActivity.VideoDetailActivity;
 import cn.edu.hebtu.software.zhilvdemo.R;
+import cn.edu.hebtu.software.zhilvdemo.Setting.MyApplication;
+import cn.edu.hebtu.software.zhilvdemo.Util.DetermineConnServer;
 
 /**
  * @ProjectName:    ZhiLv
@@ -37,17 +53,34 @@ public class TravelsRecommendFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private SmartRefreshLayout refreshLayout;
     private List<Note> mDatas = new ArrayList<>();
-    private String mTitle = "Defaut Value";
     private View view;
+    private StaggeredGridAdapter adapter;
+    private MyApplication data;
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what){
+                case 1001:
+                    Toast.makeText(getActivity().getApplicationContext(), (CharSequence)msg.obj, Toast.LENGTH_SHORT).show();
+                    break;
+                case 1002:
+                    mDatas = (List<Note>)msg.obj;
+                    adapter.replaceAll(mDatas);
+                    break;
+            }
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_travels_recommend, container, false);
         mRecyclerView = view.findViewById(R.id.travels_recommend_recycler);
         refreshLayout = view.findViewById(R.id.refresh_layout);
+        data = (MyApplication)getActivity().getApplication();
 
-//        initDatas();
         showStagger();
+        initDatas();
 
         return view;
     }
@@ -63,15 +96,17 @@ public class TravelsRecommendFragment extends Fragment {
         //设置布局管理器到RecyclerView里
         mRecyclerView.setLayoutManager(layoutManager);
         //创建适配器
-        StaggeredGridAdapter adapter = new StaggeredGridAdapter(mDatas,getActivity());
+        adapter = new StaggeredGridAdapter(mDatas,getActivity());
         adapter.setOnItemClickListener(new StaggeredGridAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(int position) {
                 if(!mDatas.get(position).isFlag()){
                     Intent intent = new Intent(getActivity(), VideoDetailActivity.class);
+                    intent.putExtra("video", mDatas.get(position).getVideo());
                     startActivity(intent);
                 }else{
                     Intent intent = new Intent(getActivity(), TravelDetailActivity.class);
+                    intent.putExtra("travels", mDatas.get(position).getTravels());
                     startActivity(intent);
                 }
             }
@@ -85,12 +120,49 @@ public class TravelsRecommendFragment extends Fragment {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-//                        adapter.replaceAll(getData());
+                        initDatas();
                         refreshLayout.finishRefresh();
                     }
                 },1000);
             }
         });
     }
+
+    private void initDatas(){
+        if(null != data.getUser()){
+            Thread thread = new getNoteList();
+            thread.start();
+        }
+    }
+
+    class getNoteList extends Thread{
+        @Override
+        public void run() {
+            try {
+                Message msg = Message.obtain();
+                if(DetermineConnServer.isConnByHttp(getActivity().getApplicationContext())) {
+                    URL url = new URL("http://"+ data.getIp()+":8080/ZhiLvProject/recommend/travels/getRecommendList?userId="+data.getUser().getUserId());
+                    URLConnection conn = url.openConnection();
+                    InputStream in = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in,"utf-8"));
+                    String info = reader.readLine();
+                    Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+                    Type type = new TypeToken<List<Note>>(){}.getType();
+                    List<Note> noteList = gson.fromJson(info,type);
+                    msg.what = 1002;
+                    msg.obj = noteList;
+                }else {
+                    msg.what = 1001;
+                    msg.obj = "未连接到服务器";
+                }
+                mHandler.sendMessage(msg);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
 }
